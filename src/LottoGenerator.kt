6eab3998931746648efd43a9c5153fdf8a92f594
천수빈 <suebin710@gmail.com>
2021-03-07 16:26:05 +0900

    add method printNumber
    배열의 요소를 출력하는 메소드

--- src/LottoGenerator.kt
+++ src/LottoGenerator.kt
@@ -1,19 +1,30 @@
 import java.util.*
 import kotlin.random.Random
 
-fun createNumber(): List<Int> {
+fun printNumber(nums: List<Int>, idx: Int) {
+    print("[ NO.${idx} ] : ")
+    for (num in nums) {
+        print("$num ")
+    }
+    println()
+}
+
+fun createNumber() : List<Int> {
     val nums = mutableListOf<Int>()
-    while(nums.size < 6){
+    while(nums.size < 7){
         val randomNumber = Random.nextInt(1, 46)
         if (!nums.contains(randomNumber)) nums.add(randomNumber)
     }
+
     return nums
 }
 
-fun printNumber(nums: List<Int>) {
-    
-}
-
 fun main() {
-    val upTo = Scanner(System.`in`).nextInt()
+    println("[ Lotto Generator ]")
+    print("횟수를 입력하세요. : ")
+    val input = Scanner(System.`in`).nextInt()
+
+    for (i in 1..input) {
+        printNumber(createNumber(), i)
+    }
 }
